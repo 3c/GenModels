7c9ac7887306853a825a5b92c91fbc72dc805228
CX <chenxi@goumin.com>
2015-08-21 16:39:27 +0800

    Initial commit

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by cx on 8/21/15.
+ */
+public class TestMain {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestMain.class);
+
+
+    public static void main(String[] args) {
+
+        logger.info("hehe{}","hhhhh");
+        logger.info("hjaha");
+
+    }
+}
